bbab4b60783ddffb655ab72f7424a756730ccc63
erik <erikhervall11@gmail.com>
2024-04-01 14:58:29 -0700

    pushing and changing branch

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -62,13 +62,14 @@
 const router = require("express").Router();
 const sessionRouter = require("./session.js");
 const usersRouter = require("./users.js");
+const spotsRouter = require("./spots.js");
 const { restoreUser } = require("../../utils/auth.js");
 
 // Connect restoreUser middleware to the API router
 // If current user session is valid, set req.user to the user in the database
 // If current user session is not valid, set req.user to null
 router.use(restoreUser);
-
+router.use("/spots", spotsRouter);
 router.use("/session", sessionRouter);
 
 router.use("/users", usersRouter);
---
+++ backend/routes/api/spots.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const { Op } = require("sequelize");
+const bcrypt = require("bcryptjs");
+
+const {
+  setTokenCookie,
+  restoreUser,
+  requireAuth,
+} = require("../../utils/auth");
+const { Spot } = require("../../db/models");
+
+const router = express.Router();
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+const spot = require("../../db/models/spot");
+
+router.get("/", async (req, res, next) => {
+  const spots = await Spot.findAll();
+  res.status(200).json(spots);
+});
+
+router.get("/current", requireAuth, async (req, res, next) => {
+  const currentUserId = req.user.id;
+  const ownedSpots = await Spot.findAll({
+    where: { ownerId: currentUserId },
+  });
+  res.status(200).json({ Spots: ownedSpots });
+});
+
+router.get(":spotId", async (req, res, next) => {
+  const { spotId } = req.params;
+  const spot = await Spot.findByPk(spotId);
+  if (!spot) {
+    return res.status(404).json({
+      message: "Spot couldn't be found",
+    });
+  }
+});
+
+module.exports = router;
