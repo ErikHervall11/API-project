8642f3fa77147573828942d1dd82b27efb71fe5e
erik <erikhervall11@gmail.com>
2024-04-01 17:07:21 -0700

    validations done for now

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -23,38 +23,97 @@ module.exports = (sequelize, DataTypes) => {
       address: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          notNull: { msg: "Street address is required" },
+          notEmpty: { msg: "Street address is required" },
+        },
       },
       city: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          notNull: { msg: "City is required" },
+          notEmpty: { msg: "City is required" },
+        },
       },
       state: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          notNull: { msg: "State is required" },
+          notEmpty: { msg: "State is required" },
+        },
       },
       country: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          notNull: { msg: "Country is required" },
+          notEmpty: { msg: "Country is required" },
+        },
       },
       lat: {
         type: DataTypes.DECIMAL,
         allowNull: false,
+        validate: {
+          isDecimal: true,
+          min: {
+            args: [-90],
+            msg: "Latitude must be within -90 and 90",
+          },
+          max: {
+            args: [90],
+            msg: "Latitude must be within -90 and 90",
+          },
+          notNull: true,
+        },
       },
       lng: {
         type: DataTypes.DECIMAL,
         allowNull: false,
+        validate: {
+          isDecimal: true,
+          min: {
+            args: [-180],
+            msg: "Longitude must be within -180 and 180",
+          },
+          max: {
+            args: [180],
+            msg: "Longitude must be within -180 and 180",
+          },
+          notNull: true,
+        },
       },
       name: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          notNull: true,
+          notEmpty: true,
+          len: {
+            args: [0, 50],
+            msg: "Name must be less than 50 characters",
+          },
+        },
       },
       description: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          notNull: { msg: "Description is required" },
+          notEmpty: { msg: "Description is required" },
+        },
       },
       price: {
         type: DataTypes.DECIMAL,
         allowNull: false,
+        validate: {
+          isDecimal: true,
+          min: {
+            args: [0],
+            msg: "Price per day must be a positive number",
+          },
+        },
       },
       createdAt: {
         type: DataTypes.DATE,
