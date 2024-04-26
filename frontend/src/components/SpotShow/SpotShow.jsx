7c069055a493cb7f4cdf07d8c9def3f1163942d9
erik <erikhervall11@gmail.com>
2024-04-26 12:12:51 -0700

    only one more

--- frontend/src/components/SpotShow/SpotShow.jsx
+++ frontend/src/components/SpotShow/SpotShow.jsx
@@ -42,27 +42,27 @@ function SpotShow() {
 
               <img
                 id="spotImage-large"
-                src={spot.SpotImages[0].url}
+                src={spot?.SpotImages?.[0]?.url}
                 alt="image 1"
               />
               <img
                 id="spotImage-small"
-                src={spot.SpotImages[1].url}
+                src={spot?.SpotImages?.[1]?.url}
                 alt="image 2"
               />
               <img
                 id="spotImage-small"
-                src={spot.SpotImages[2].url}
+                src={spot?.SpotImages?.[2]?.url}
                 alt="image 3"
               />
               <img
                 id="spotImage-small"
-                src={spot.SpotImages[3].url}
+                src={spot?.SpotImages?.[3]?.url}
                 alt="image 4"
               />
               <img
                 id="spotImage-small"
-                src={spot.SpotImages[4].url}
+                src={spot?.SpotImages?.[4]?.url}
                 alt="image 5"
               />
             </div>
